bea0d26c35bd87c71118cb9df422dc725e933511
aaakch0316 <aaakch0316@gmail.com>
2021-08-10 16:58:00 +0900

    create [+] navbar html, css

--- src/component/navbar/NavCss.jsx
+++ src/component/navbar/NavCss.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import "./NavCss.css"
 
 const NavCss = () => {
     return (
-        <div>navbar</div>
+        <div id="wrapper">
+            <a href="#main" id="skip-nav">skip navigation</a>
+            <header id="header">
+            <h1><a href="#">PLMS</a></h1>
+            <nav id="gnb">
+                <ul>
+                <li>
+                    <a href="#" class="big_gnb">Punch List</a>
+                    <ul>
+                    <li><a href="#">All Item</a></li>
+                    <li><a href="#">My Item</a></li>
+                    <li><a href="#">Recycle Bin</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#" class="big_gnb">Management</a>
+                </li>
+                </ul>
+            </nav>
+            </header>
+        </div>
     )
 }
 
