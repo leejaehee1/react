ecda2db7b653588cdab3a65e0d3d0ce485974043
aaakch0316 <aaakch0316@gmail.com>
2021-08-17 14:15:06 +0900

    material-ui [+] init react-admin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { Admin } from 'react-admin';
+import fakeDataProvider from 'ra-data-fakerest';
+
 import NavCss from './component/navbar/NavCss';
 import AppRouter from './component/router/RouterComponent';
 import { BrowserRouter} from 'react-router-dom';
 
+const dataProvider = fakeDataProvider({
+  posts: [
+      { id: 0, title: 'Hello, world!' },
+      { id: 1, title: 'FooBar' },
+  ],
+  comments: [
+      { id: 0, post_id: 0, author: 'John Doe', body: 'Sensational!' },
+      { id: 1, post_id: 0, author: 'Jane Doe', body: 'I agree' },
+  ],
+})
 
 function App() {
   return (
-    <div className="App">
+    <Admin
+      dataProvider={dataProvider}  
+    >
       <NavCss />
       <BrowserRouter>
         <AppRouter />
      </BrowserRouter> 
-    </div>
+    </Admin>
   );
 }
 
