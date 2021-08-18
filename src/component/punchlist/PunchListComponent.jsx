71eeebd76466caf541b2305b283c71a6c78a1d9c
aaakch0316 <aaakch0316@gmail.com>
2021-08-18 17:06:00 +0900

    Material-ui [+] base layout grid

--- src/component/punchlist/PunchListComponent.jsx
+++ src/component/punchlist/PunchListComponent.jsx
@@ -16,12 +16,31 @@
 // export default PunchListComponent;
 
 import * as React from "react";
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import { Title } from 'react-admin';
-export default () => (
-    <Card>
-        <Title title="Welcome to the administration" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-    </Card>
-);
+import { Box } from '@material-ui/core';
+// import Card from '@material-ui/core/Card';
+// import CardContent from '@material-ui/core/CardContent';
+// import { Title } from 'react-admin';
+const PunchListComponent = () => {
+
+    return (
+        <>
+            {/* <Card>
+                <Title title="Welcome to the administration" />
+            <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+            </Card> */}
+            <Box display="flex" mt="3em"> 
+                <Box bgcolor="palevioletred" flex="4" mr="4em">
+                    {/* <DealsChart /> */}adfasdf
+                </Box>
+                <Box bgcolor="palevioletred" flex="4" mr="4em">
+                    {/* <Welcome /> */}asdfasdf
+                </Box>
+                <Box bgcolor="palevioletred" flex="4">
+                    {/* <Welcome /> */}asdfasdf
+                </Box>
+            </Box>
+        </>
+    )
+};
+
+export default PunchListComponent;
