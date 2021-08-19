51fa4ea7818c99339b6ebb31a4d5c2bbf772a30b
aaakch0316 <aaakch0316@gmail.com>
2021-08-20 08:17:34 +0900

    Authentication [+] login test init

--- src/component/authentication/component/LoginPage.jsx
+++ src/component/authentication/component/LoginPage.jsx
@@ -6,14 +6,14 @@ import { Login } from 'react-admin';
 import LoginSide from './LoginSide';
 
 class LoginPage extends Component {
-    // submit = (e) => {
-    //     e.preventDefault();
-    //     // gather your data/credentials here
-    //     const credentials = { };
+    submit = (e) => {
+        e.preventDefault();
+        // gather your data/credentials here
+        const credentials = { };
 
-    //     // Dispatch the userLogin action (injected by connect)
-    //     this.props.userLogin(credentials);
-    // }
+        // Dispatch the userLogin action (injected by connect)
+        this.props.userLogin(credentials);
+    }
 
     // render() {
     //     return (
@@ -37,4 +37,48 @@ class LoginPage extends Component {
     )}
 };
 
+
 export default connect(undefined, { userLogin })(LoginPage);
+
+
+import * as React from 'react';
+import { useState } from 'react';
+import { useLogin, useNotify, Notification, defaultTheme } from 'react-admin';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+function LoginPage({ theme }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const login = useLogin();
+    const notify = useNotify();
+    const submit = e => {
+        e.preventDefault();
+        // will call authProvider.login({ email, password })
+        login({ email, password }).catch(() =>
+            notify('Invalid email or password')
+        );
+    };
+
+    return (
+        <>
+            <form onSubmit={submit}>
+                <input
+                    name="email"
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+            </form>
+            <Notification />
+        </>
+    );
+};
+
+export default LoginPage;
