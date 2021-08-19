a6e6408146a740e057c2164d145821966effb427
aaakch0316 <aaakch0316@gmail.com>
2021-08-19 10:41:32 +0900

    Error [-] chart customiztion error

--- src/component/punchlist/chart/Category.jsx
+++ src/component/punchlist/chart/Category.jsx
@@ -14,28 +14,52 @@ const fruits = [
     { fruit: 'Pears', count: 20 },
     { fruit: 'Pineapples', count: 3 }
 ];
- 
+
+function onLegendClick(e) {
+    console.log(e.points)
+}
+
+
+// https://js.devexpress.com/Demos/WidgetsGallery/Demo/Charts/Pie/React/Light/
+function pointClickHandler(e) {
+    toggleVisibility(e.target);
+}
+
+function legendClickHandler(e) {
+    let arg = e.target;
+    let item = e.component.getAllSeries()[0].getPointsByArg(arg)[0];
+    toggleVisibility(item);
+}
+
+function toggleVisibility(item) {
+    item.isVisible() ? item.hide() : item.show();
+}
+
 export default function Category() {
     return (
         <PieChart 
             dataSource={fruits}
-            // title="I am the Title"
         >
             <Series argumentField="fruit" valueField="count" />
             <Tooltip enabled={true} />
-            {/* <Title text="I am the Test">
-                    <Subtitle text="I am the Subtitle" />
-            </Title> */}
             <Legend
+                    orientation="vertical"
                     // center/right/left
                     horizontalAlignment="right" 
                     // top/bottom
                     verticalAlignment="top"
+                    
+                    // columnItemSpacing={20}
+                    // rowItemSpacing={30}
+
+
+                    onPointClick={pointClickHandler}
+                    onLegendClick={legendClickHandler}
                 />
             <AdaptiveLayout
-                    height={10}
-                    width={50}
-                />
+                    // height={10}
+                    // width={50}
+            />
         </PieChart>
     );
 }
