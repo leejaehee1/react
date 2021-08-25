7f4ed57af8b23bfa4ab5d695f28d8645e265342a
aaakch0316 <aaakch0316@gmail.com>
2021-08-25 22:40:34 +0900

    Material-ui [+] add punchs status-icon

--- src/component/punchlist/Punchs.jsx
+++ src/component/punchlist/Punchs.jsx
@@ -25,6 +25,7 @@ import {
     ListItemText,
     Checkbox,
     Typography,
+    Box,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
@@ -72,6 +73,7 @@ const PunchListContent = () => {
                                 <>
                                     {contact.IssueDesp} 
                                     {/* at{' '} */}
+                                    <br />
                                     {contact.CompleteDesp}
                                     {/* <ReferenceField
                                         record={contact}
@@ -88,6 +90,58 @@ const PunchListContent = () => {
                                 </>
                             }
                         />
+{/* 
+
+    import * as React from 'react';
+    import { Box } from '@material-ui/core';
+
+    const getColorFromStatus = (status: string) =>
+        status === 'cold'
+            ? '#7dbde8'
+            : status === 'warm'
+            ? '#e8cb7d'
+            : status === 'hot'
+            ? '#e88b7d'
+            : status === 'in-contract'
+            ? '#a4e87d'
+            : '#000';
+
+    export const Status = ({ status }: { status: string }) => (
+        <Box
+            width={10}
+            height={10}
+            display="inline-block"
+            borderRadius={5}
+            bgcolor={getColorFromStatus(status)}
+            component="span"
+        />
+);
+
+
+ */}
+
+                        <ListItemSecondaryAction>
+                            <Typography variant="body2" color="textSecondary">
+                                {/* last activity{' '} */}
+                                {/* {formatDistance(
+                                    new Date(contact.last_seen),
+                                    now
+                                )}{' '} */}
+                                {/* ago <Status status={contact.status} /> */}
+                                <Box
+                                    width="90px"
+                                    height={28}
+                                    display="inline-block"
+                                    textAlign="center"
+                                    lineHeight="28px"
+                                    verticalAlign="middle"
+                                    color="white"
+                                    borderRadius={3}
+                                    bgcolor='#e88b7d'
+                                    component="span"
+                                >Open</Box>
+                            </Typography>
+                        </ListItemSecondaryAction>
                     </ListItem>
                 );
             })}
