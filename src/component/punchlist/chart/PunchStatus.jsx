4159aaa75b421e0d0caaf36057ed55e9f130f8a5
aaakch0316 <aaakch0316@gmail.com>
2021-08-30 14:34:57 +0900

    DataFlow [+] add Piechart data

--- src/component/punchlist/chart/PunchStatus.jsx
+++ src/component/punchlist/chart/PunchStatus.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 import Paper from '@material-ui/core/Paper';
 import {
@@ -16,6 +16,9 @@ import {
 
 import { useGetOne, useGetList, SimpleList, useGetIdentity, useGetMany } from 'react-admin';
 import { func } from "prop-types";
+import { number } from "ra-language-french/node_modules/ra-core";
+
+
 
 
 // function countStatus(num) {
@@ -43,137 +46,109 @@ import { func } from "prop-types";
 //       // console.log('Mangoes and papayas are $2.79 a pound.');
 //       // expected output: "Mangoes and papayas are $2.79 a pound."
 //       break;
-//     default:
-//       console.log(`Sorry, we are out of ${data[id].status}.`);
-//       break;
+    // default:
+    //   console.log(`Sorry, we are out of ${data[id].status}.`);
+    //   break;
 //   }
 // }
 
 
 // API : https://devexpress.github.io/devextreme-reactive/react/chart/docs/reference/pie-series/
+
+// function AddCC() {
+//   const [opened, setOpened] = useState(0);
+//   const [readyForReview, setReadyForReview] = useState(0);
+//   const [requestedForClose, setRequestedForClose] = useState(0);
+//   const [notAccepted, setNotAccepted] = useState(0);
+//   const [closed, setClosed] = useState(0)
+  
+//   // console.log(opened, readyForReview, requestedForClose, notAccepted, closed)
+//   const { data, ids, loading, error } = useGetList('list', );
+//   console.log("addCC 들어옴")
+  
+//   useEffect((
+    
+//     ) =>{
+//   for (const id in data) {
+//     // addCC()
+//     console.log(parseInt(data[id].status))
+//     // countStatus(parseInt(data[id].status))
+//     switch (parseInt(data[id].status)) {
+//       case 2:
+//         setOpened(opened => opened+1)
+//         console.log("------------------")
+//         break;
+//       case 3:
+//         setReadyForReview(readyForReview+1)
+//         console.log("----------------------------------")
+//         break;
+//       case 4:
+//         setRequestedForClose(requestedForClose+1)
+//         console.log("---------------------------------------------")
+//         break;
+//       case 5:
+//         setNotAccepted(notAccepted+1)
+//         console.log("---------------------------------------------------------")
+//         break;
+//       case 6:
+//         setClosed(closed+1)
+//         console.log("-----------------------------------------------------------------------")
+//         break;
+//       default:
+//         console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+//         break;
+//       }
+//     }}
+//     , [])
+//   return (
+//     <>
+//       <p>{opened}</p>
+//       <p>{readyForReview}</p>
+//       <p>{requestedForClose}</p>
+//       <p>{notAccepted}</p>
+//       <p>{closed}</p>
+//     </>
+//   )
+// }
+
+
+
+
+
+
 const PunchStatus = () => {
-  
-  const [opened, setOpened] = useState(0);
-  const [readyForReview, setReadyForReview] = useState(0);
-  const [requestedForClose, setRequestedForClose] = useState(0);
-  const [notAccepted, setNotAccepted] = useState(0);
-  const [closed, setClosed] = useState(0)
-  
-  // console.log(opened, readyForReview, requestedForClose, notAccepted, closed)
+  const [toCharts, setToCharts] = useState([])
   const { data, ids, loading, error } = useGetList('list', );
+  const targetData = data
+
+
+  const a = Object.values(targetData).filter(da => da.status==="2").length;
+  const b = Object.values(targetData).filter(da => da.status==="3").length;
+  const c = Object.values(targetData).filter(da => da.status==="4").length;
+  const d = Object.values(targetData).filter(da => da.status==="5").length;
+  const e = Object.values(targetData).filter(da => da.status==="6").length;
+
   
   
-  function addCC() {
-    setOpened(opened+1)
-  }
-
-  function countStatus(num) {
-    if (num===2) {
-      addCC()
-    }
-    addCC()
-    switch (num) {
-      case 2:
-        // setOpened(opened+1)
-        addCC()
-        console.log("------------------")
-        // console.log(opened)
-        break;
-      case 3:
-        // console.log(readyForReview);
-        setReadyForReview(readyForReview+1)
-        // console.log(readyForReview);
-        console.log("----------------------------------")
-        break;
-      case 4:
-        setRequestedForClose(requestedForClose+1)
-        console.log("---------------------------------------------")
-        break;
-      case 5:
-        setNotAccepted(notAccepted+1)
-        console.log("---------------------------------------------------------")
-        break;
-      case 6:
-        setClosed(closed+1)
-        console.log("-----------------------------------------------------------------------")
-        // console.log('Mangoes and papayas are $2.79 a pound.');
-        // expected output: "Mangoes and papayas are $2.79 a pound."
-        break;
-      default:
-        // console.log(`Sorry, we are out of ${data[id].status}.`);
-        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-        break;
-    }
-  }
-
   useEffect(() => {
-    console.log("useEffectuseEffectuseEffectuseEffectuseEffectuseEffectuseEffectuseEffectuseEffectuseEffectuseEffect")
-    for (const id in data) {
-      // addCC()
-      console.log(parseInt(data[id].status))
-      // console.log(opened, readyForReview, requestedForClose, notAccepted, closed)
-      // countStatus(parseInt(data[id].status))
-      // console.log(opened, readyForReview, requestedForClose, notAccepted, closed)
-      // setOpened(opened+1)
-      // console.log(opened, readyForReview, requestedForClose, notAccepted, closed)
+    // ActionLogic()
+    // console.log('마운트 될 때만 실행됩니다.');
+  }, [data]);
 
-      console.log('!!!!!!!!!ㅐㅑㄷㅂㄷㅂㅈㄱㅈㄷㅂㄱㅂㅈㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱㅈㅂㄷㄱㅂㅈㄷㄱㅂㅈㄷㄱ')
 
-      if (parseInt(data[id].status)===2) {
-        addCC()
-        console.log('먹히니 안먹히니')
-
-      }
-      // countStatus(data[id].status)
-    }  
-  }, [])
-  // console.log(opened)
-  // console.log(readyForReview)
-  // console.log(requestedForClose)
-  // console.log(notAccepted)
-  // console.log(closed)
-
+  const pieData = [
+    { argument: "opened", value: a },
+    { argument: "Ready for Review", value: b },
+    { argument: "requested for close", value: c },
+    { argument: "not accepted", value: d },
+    { argument: "closed", value: e },
+  ];
   
-  // console.log(ids, data)
-  console.log("---------------------------------------------------------------")
-  // for (const id in data) {
-  //   console.log(`${data[id].status}`)
-  //   countStatus(data[id].status)
-  // }
-  // console.log(Object.keys(data).length)
-  // console.log(typeof(data))
-  // console.log(data.length)
-  // const { data : punchID, loading, error } = useGetMany(
-                                                          // 'list', );
-
-
-  // const data = [ 
-  //   { argument:'Monday', value:10 },
-  //   { argument:'Tuesday', value:40 },
-  //   { argument:'Wednesday', value:10 },
-  //   { argument:'Thursday', value:20 },
-  //   { argument:'Friday', value:20 },
-  // ];
   return (
     <Paper>
-      <p>{opened}</p>
-      <p>{readyForReview}</p>
-      <p>{requestedForClose}</p>
-      <p>{notAccepted}</p>
-      <p>{closed}</p>
-        asdfasdfasdf
-        {/* {punchID} */}
-          <button onClick={addCC}>buttttttooooon</button>
-        <ul>
-            {ids.map(id =>
-                <li key={id}>
-                  {/* {setOpened(opened+1)}, */}
-                  {data[id].status}
-                </li>
-            )}
-        </ul>
-      {/* <Chart
-        data={data}
+
+      <Chart
+        data={pieData}
       //   width={100}
         height={220}
         rotated={true}
@@ -186,16 +161,17 @@ const PunchStatus = () => {
           valueField="value" 
           argumentField="argument" 
           innerRadius={0.3}
-          outerRadius={0.6} 
+          outerRadius={0.8} 
           />
           <EventTracker />
           <HoverState />
-          <Animation /> */}
+          <Animation />
         {/* <Title text="Studies per day"/> */}
-      {/* </Chart> */}
+      </Chart>
       
     </Paper>
   );
 }
   
+
 export default PunchStatus;
