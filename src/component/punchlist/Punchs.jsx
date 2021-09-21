c5cb9b1714692e594a776a2169bd40d1519dd761
aaakch0316 <aaakch0316@gmail.com>
2021-09-21 13:30:17 +0900

    PL-111 [+] add button design at punchlist

--- src/component/punchlist/Punchs.jsx
+++ src/component/punchlist/Punchs.jsx
@@ -14,7 +14,7 @@ import {
     Pagination,
     // useGetIdentity,
     // TextField,
-    Button,
+    // Button,
     ListActions,
 
     // Datagrid,
@@ -23,6 +23,7 @@ import {
 import {
     List,
     ListItem,
+    
     // ListItemAvatar,
     ListItemIcon,
     ListItemSecondaryAction,
@@ -30,88 +31,148 @@ import {
     Checkbox,
     Typography,
     Box,
+    styled,
+    Button,
+    Grid,
+    Card,
+    // buttonUnstyledClasses,
+    // ButtonUnstyled,
+    // CustomButton,
 } from '@material-ui/core';
+import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/core/ButtonUnstyled';
+// import {Paper} from '@material-ui/core';
+
+
+
+
 import { cloneElement } from 'react';
 import IconEvent from '@material-ui/icons/Event';
+// import Button from '@mui/material/Button';
+
+
+const CustomButtonRoot = styled('span')(`
+  background-color: #007fff;
+  padding: 15px 20px;
+  border-radius: 10px;
+  color: #fff;
+  font-weight: 600;
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  transition: all 200ms ease;
+  cursor: pointer;
+  box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 0 rgba(0, 127, 255, 0);
+  border: none;
+
+  &:hover {
+    background-color: #0059b2;
+  }
+
+  &.${buttonUnstyledClasses.active} {
+    background-color: #004386;
+  }
+
+  &.${buttonUnstyledClasses.focusVisible} {
+    box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
+    outline: none;
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+    box-shadow: 0 0 0 0 rgba(0, 127, 255, 0);
+  }
+`);
+
+function CustomButton(props) {
+  return <ButtonUnstyled {...props} component={CustomButtonRoot} />;
+}
 
 const PunchListContent = () => {
     const { data, ids, onToggleItem, } = useListContext();
     return (
-        <List>
-            aaa
-            {ids.map(id => {
-                const contact = data[id];;
-                return (
-                    <ListItem
-                        // button
-                        key={id}
-                        // component={Link}
-                        // to={`/contacts/${id}/show`}
-                    >
-                        <ListItemIcon>
-                            <Checkbox
-                                edge="start"
-                                // checked={selectedIds.includes(id)}
-                                // tabIndex={-1}
-                                // disableRipple
-                                onClick={e => {
-                                    // e.stopPropagation();
-                                    console.log(e.target)
-                                    console.log(id)
-                                    onToggleItem(id);
-                                }}
+        // <Card>
+            <List dense={true}  >
+                {ids.map(id => {
+                    const contact = data[id];;
+                    return (
+                        <>
+                        <ListItem
+                            button
+                            key={id}
+                            // component={Link}
+                            // to={`/contacts/${id}/show`}
+                        >
+                            <ListItemIcon>
+                                <Checkbox
+                                    edge="start"
+                                    // checked={selectedIds.includes(id)}
+                                    // tabIndex={-1}
+                                    // disableRipple
+                                    onClick={e => {
+                                        // e.stopPropagation();
+                                        console.log(e.target)
+                                        console.log(id)
+                                        onToggleItem(id);
+                                    }}
+                                />
+                            </ListItemIcon>
+                            <ListItemText 
+                                primary={`${contact.punchID}, ${contact.discipline}, ${contact.bulkName}, ${contact.scheduleImpact}, ${contact.completedBy}`}
+                                secondary={
+                                    <>
+                                        {contact.issueDescription} 
+                                        <br />
+                                        {contact.completeComment}
+                                        {/* <ReferenceField
+                                            record={contact}
+                                            source="company_id"
+                                            reference="companies"
+                                            basePath="/companies"
+                                            link={false}
+                                            >
+                                            <TextField source="IssueDesp" />
+                                        </ReferenceField>{' '} */}
+                                        {/* {contact.nb_notes &&
+                                            `- ${contact.nb_notes} notes `} */}
+                                        {/* <TagsList record={contact} /> */}
+                                    </>
+                                }
                             />
-                        </ListItemIcon>
-                        <ListItemText 
-                            primary={`${contact.punchID}, ${contact.discipline}, ${contact.bulkName}, ${contact.scheduleImpact}, ${contact.completedBy}`}
-                            secondary={
-                                <>
-                                    {contact.issueDescription} 
-                                    <br />
-                                    {contact.completeComment}
-                                    {/* <ReferenceField
-                                        record={contact}
-                                        source="company_id"
-                                        reference="companies"
-                                        basePath="/companies"
-                                        link={false}
-                                        >
-                                        <TextField source="IssueDesp" />
-                                    </ReferenceField>{' '} */}
-                                    {/* {contact.nb_notes &&
-                                        `- ${contact.nb_notes} notes `} */}
-                                    {/* <TagsList record={contact} /> */}
-                                </>
-                            }
-                        />
 
 
-                        <ListItemSecondaryAction>
-                            <Typography variant="body2" color="textSecondary">
-                                {/* last activity{' '} */}
-                                {/* {formatDistance(
-                                    new Date(contact.last_seen),
-                                    now
-                                )}{' '} */}
-                                {/* ago <Status status={contact.status} /> */}
-                                <Box
-                                    width="90px"
-                                    height={28}
-                                    display="inline-block"
-                                    textAlign="center"
-                                    lineHeight="28px"
-                                    verticalAlign="middle"
-                                    color="white"
-                                    borderRadius={3}
-                                    bgcolor='#e88b7d'
-                                    component="span"
-                                >Open</Box>
-                            </Typography>
-                        </ListItemSecondaryAction>
-                    </ListItem>
-                );
-            })}
-        </List>
+                            <ListItemSecondaryAction>
+                                <Typography variant="body2" color="textSecondary">
+                                    {/* last activity{' '} */}
+                                    {/* {formatDistance(
+                                        new Date(contact.last_seen),
+                                        now
+                                    )}{' '} */}
+                                    {/* ago <Status status={contact.status} /> */}
+                                    <Box
+                                        width="90px"
+                                        height={28}
+                                        display="inline-block"
+                                        textAlign="center"
+                                        lineHeight="28px"
+                                        verticalAlign="middle"
+                                        color="white"
+                                        borderRadius={3}
+                                        bgcolor='#e88b7d'
+                                        component="span"
+                                    >Open</Box>
+                                </Typography>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                        <hr />
+
+                        </>
+                    );
+                    
+                }
+                )
+            }
+            </List>
+        // </Card>
     )
 }
 
@@ -126,7 +187,26 @@ export const Punchs = props => {
                 onClick={() => { alert('Your custom action'); }}
                 // label="Show calendar"
             >
-                {/* <IconEvent/> */}
+                <IconEvent/>
+            </Button>
+            <Button>test <br /> button</Button>
+            <Grid spacing={2} direction="row">
+                <CustomButton>Button</CustomButton>
+                <CustomButton disabled>Disabled</CustomButton>
+            </Grid>
+            <button>test <br /> button</button>
+            <button>test <br /> button</button>
+            <button>test <br /> button</button>
+            <button>test <br /> button</button>
+            <button>test <br /> button</button>
+            <button>test <br /> button</button>
+            <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        // disabled={!text || loading}
+                    >
+                        Add this note
             </Button>
         </TopToolbar>
     );
