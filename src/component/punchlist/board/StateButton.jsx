8b5df3b6f1a95a94e862d1435d87b7b821184f86
aaakch0316 <aaakch0316@gmail.com>
2021-09-21 14:21:01 +0900

    PL-107 [+] apply material-ui Menus at punchlist borard UI statebutton

--- src/component/punchlist/board/StateButton.jsx
+++ src/component/punchlist/board/StateButton.jsx
@@ -1,25 +1,83 @@
 import React from 'react';
 import {
     Box,
+    Menu,
+    MenuItem,
+    Button,
 } from '@material-ui/core';
 
-const StateButton = () => {
+const options = [
+    'Show some love to Material-UI',
+    'Show all notification content',
+    'Hide sensitive notification content',
+    'Hide all notification content',
+  ];
 
+const StateButton = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+    const handleClickListItem = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <>
-            <Box
-                width="90px"
-                height={28}
-                display="inline-block"
-                textAlign="center"
-                lineHeight="28px"
-                verticalAlign="middle"
-                color="white"
-                borderRadius={3}
-                bgcolor='#e88b7d'
-                component="span"
-            >Open</Box>
-        </>
+        <div>
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClickListItem}>
+                <Box
+                    button
+                    width="90px"
+                    height={28}
+                    display="inline-block"
+                    textAlign="center"
+                    lineHeight="28px"
+                    verticalAlign="middle"
+                    color="white"
+                    borderRadius={3}
+                    bgcolor='#e88b7d'
+                    component="span"
+                >Open</Box>
+                {/* Open Menu */}
+            </Button>
+            <Menu
+                id="lock-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                {options.map((option, index) => (
+                <MenuItem
+                    key={option}
+                    disabled={index === 0}
+                    selected={index === selectedIndex}
+                    onClick={(event) => handleMenuItemClick(event, index)}
+                >
+                    <Box
+                        width="90px"
+                        height={28}
+                        display="inline-block"
+                        textAlign="center"
+                        lineHeight="28px"
+                        verticalAlign="middle"
+                        color="white"
+                        borderRadius={3}
+                        bgcolor='#e88b7d'
+                        component="span"
+                    >Open</Box>
+                    {option}
+                </MenuItem>
+                ))}
+            </Menu>
+        </div>
     )
 }
 
