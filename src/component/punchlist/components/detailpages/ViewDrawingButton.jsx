5cd4300de0b6115f1cf8d6ab95ab2bb55802c093
aaakch0316 <aaakch0316@gmail.com>
2021-09-27 19:08:27 +0900

    PL-129 [+] create A4 size viewDrawing baseline at punchlist

--- src/component/punchlist/components/detailpages/ViewDrawingButton.jsx
+++ src/component/punchlist/components/detailpages/ViewDrawingButton.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
+import Modal from '@material-ui/core/Modal';
 import './styles/viewdrawingbutton.css'
 
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+    // return 0
+}
+
+function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
 const ViewDrawingButton = () => {
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
         <>
-            <button className="ViewDrawingButton">View Drawing</button>
+            <button className="ViewDrawingButton" onClick={handleOpen}>
+                View Drawing
+            </button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {/* {body} */}
+                <div style={modalStyle} className="page">
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                    <p>abcabcaaaaaaaaaaaaaaaaaabc</p>
+                </div>
+            </Modal>
         </>
     )
 }
