7694155c207732e076a37962b61ae5028cdb041f
aaakch0316 <aaakch0316@gmail.com>
2021-09-29 15:12:28 +0900

    PL-148 [+] upgrade DateColumn update UI at management

--- src/component/management/managementcomponent/inputComponent/ClosedDate.jsx
+++ src/component/management/managementcomponent/inputComponent/ClosedDate.jsx
@@ -72,58 +72,85 @@ const ClosedDate = (props) => {
     const classes = useStyles();
 
     // const [selectedDate, setSelectedDate] = React.useState(new Date());
-    const [selectedDate, setSelectedDate] = React.useState(props.static);
+    const [selectedDate, setSelectedDate] = React.useState(props.static?props.static:""); // 빈값으로 들어오면 빨강 값이 뜨게 한다.
     // const [selectedDate, setSelectedDate] = React.useState(new Date('2021-07-18T21:11:54'));
 
     const handleDateChange = (date) => {
       if (props.rowIndex === "targetDate") {
         if (new Date() >= date) {
-          alert("오늘 이 후의 날짜를 선택하세요")
+          alert("오늘 이 후의 날짜를 선택하세요");
+          setSelectedDate(new Date());
         } else {
+          // console.log(date)  // Thu Jan 08 1970 09:00:00 GMT+0900 (한국 표준시)
           setSelectedDate(date);
         }
       } else {
+        // console.log(date)
         setSelectedDate(date);
       }
     };
     return (
-        <>
-          <Grid container spacing={3} alignItems="center">
-            <Grid item xs={3}>
-            {/* elevation={0} */}
-              <Paper elevation={0} className={classes.lpaper}>
-                {/* <ThemeProvider theme={theme}>
-                  <Typography variant="h5">Target Date</Typography>
-                </ThemeProvider> */}
-                {props.rowName} :
-              </Paper>
-            </Grid>
-            <Grid item xs={9} spacing={-2}>
-              <Paper elevation={0} className={classes.rpaper}>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                  {/* // https://material-ui-pickers.dev/api/DatePicker */}
-                  <ThemeProvider theme={defaultMaterialTheme}>
-                    <KeyboardDatePicker
-                      disableToolbar
-                      // variant="inline"
-                      format="yyyy/MM/dd"
-                      //inputVariant="filled"  // outlined // standard
-                      // margin="normal"
-                      id={props.rowIndex}
-                      // label="Date picker inline"
-                      value={selectedDate}
-                      onChange={handleDateChange}
-                      KeyboardButtonProps={{
-                        'aria-label': 'change date',
-                      }}
-                    />
-                  </ThemeProvider>
-                </MuiPickersUtilsProvider>
-              </Paper>
-            </Grid>
-          </Grid>
-        </>
+        <div style={{paddingTop:"5px", paddingBottom:"5px"}}>
+          <span style={{}}>
+            {props.rowName} &nbsp;&nbsp; : &nbsp;&nbsp;&nbsp;&nbsp; 
+          </span>
+          <span style={{float:'right'}}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              {/* // https://material-ui-pickers.dev/api/DatePicker */}
+              <ThemeProvider theme={defaultMaterialTheme}>
+                <KeyboardDatePicker
+                  disableToolbar
+                  // variant="inline"
+                  format="yyyy/MM/dd"
+                  //inputVariant="filled"  // outlined // standard
+                  // margin="normal"
+                  id={props.rowIndex}
+                  // label="Date picker inline"
+                  value={selectedDate}  // input value : "2020/01/11"
+                  onChange={handleDateChange}
+                  KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
+                />
+              </ThemeProvider>
+            </MuiPickersUtilsProvider>
+          </span>
+        </div>
     )
 }
+          // <Grid container spacing={3} alignItems="center">
+          //   <Grid item xs={3}>
+          //   {/* elevation={0} */}
+          //     <Paper elevation={0} className={classes.lpaper}>
+          //       {/* <ThemeProvider theme={theme}>
+          //         <Typography variant="h5">Target Date</Typography>
+          //       </ThemeProvider> */}
+          //       {props.rowName} :
+          //     </Paper>
+          //   </Grid>
+          //   <Grid item xs={9} spacing={-2}>
+          //     <Paper elevation={0} className={classes.rpaper}>
+          //       <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          //         {/* // https://material-ui-pickers.dev/api/DatePicker */}
+          //         <ThemeProvider theme={defaultMaterialTheme}>
+          //           <KeyboardDatePicker
+          //             disableToolbar
+          //             // variant="inline"
+          //             format="yyyy/MM/dd"
+          //             //inputVariant="filled"  // outlined // standard
+          //             // margin="normal"
+          //             id={props.rowIndex}
+          //             // label="Date picker inline"
+          //             value={selectedDate}
+          //             onChange={handleDateChange}
+          //             KeyboardButtonProps={{
+          //               'aria-label': 'change date',
+          //             }}
+          //           />
+          //         </ThemeProvider>
+          //       </MuiPickersUtilsProvider>
+          //     </Paper>
+          //   </Grid>
+          // </Grid>
 
 export default ClosedDate;
