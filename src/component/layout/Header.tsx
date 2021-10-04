b85e0014c97355cfac6d134dd2a7ed827fee47d9
aaakch0316 <aaakch0316@gmail.com>
2021-10-04 22:43:31 +0900

    Nav [+] change base navbar UI baseline

--- src/component/layout/Header.tsx
+++ src/component/layout/Header.tsx
@@ -1,3 +1,4 @@
+import './styles/header.css'
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Tabs, Tab, Toolbar, AppBar, Box, Typography, Divider } from '@material-ui/core';
@@ -32,86 +33,112 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
     const classes = useStyles();
-    // const match = useRouteMatch(['/contacts', '/companies', '/deals']);
     const match = useRouteMatch(['/list', '/admin', '/code']);
     const currentPath = match?.path ?? '/';
-    // console.log(match)   // 객체 들어옴
-    // console.log(match.path) // error
-    // console.log(currentPath)   //  /admin # 뒷부분 들어옴
-    // const currentPath = '/';
+
 
     return (
-        <nav className={classes.root}>
-            <AppBar position="static" color="primary">
-                <Toolbar className={classes.lnav} variant="dense">
-                    <Box flex={1} display="flex" justifyContent="space-between">
-                        <Box display="flex" className={classes.lnav} flex="3" alignItems="center">
-                            <img
-                                className={classes.logo}
-                                src={
-                                    'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
-                                    // http://www.daewooenc.com/Resources/Kor/images/common/logo.png
-                                }
-                                alt="Bosch Logo"
-                            />
-                            <Typography component="span" variant="h4">
-                                P.L.M.S
-                            </Typography>
-                            <Divider className={classes.nlogo} variant="middle" orientation="vertical" />
-                        </Box>
-                        <Box display="flex" flex="5" className={classes.mnav} alignItems="center">
-                            <Typography component="span" variant="h6">
-                                &nbsp;&nbsp;&nbsp;Algerial RDPP
-                            </Typography>
-                        </Box>
-                        <Box flex="4">
-                            <Tabs
-                                value={currentPath}
-                                aria-label="Navigation Tabs"
-                            >
-                                <Tab
-                                    label={'Punch LIST'}
-                                    component={Link}
-                                    to="/"
-                                    value="/"
-                                />
-                                <Tab
-                                    label={'Management'}
-                                    component={Link}
-                                    to="/admin"
-                                    value="/admin"
-                                />
-                                <Tab
-                                    label={'Code'}
-                                    component={Link}
-                                    to="/code"
-                                    value="/code"
-                                />
-                                {/* <T  ab
-                                    label={'Companies'}
-                                    component={Link}
-                                    to="/companies"
-                                    value="/companies"
-                                />
-                                <Tab
-                                    label={'Deals'}
-                                    component={Link}
-                                    to="/deals"
-                                    value="/deals"
-                                /> */}
-                            </Tabs>
-                        </Box>
-                        <Box display="flex">
-                            <LoadingIndicator />
+        // <nav className={classes.root}>
+        //     <AppBar position="static" color="primary">
+        //         <Toolbar className={classes.lnav} variant="dense">
+        //             <Box flex={1} display="flex" justifyContent="space-between">
+        //                 <Box display="flex" className={classes.lnav} flex="3" alignItems="center">
+        //                     <img
+        //                         className={classes.logo}
+        //                         src={
+        //                             'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+        //                             // http://www.daewooenc.com/Resources/Kor/images/common/logo.png
+        //                         }
+        //                         alt="Bosch Logo"
+        //                     />
+        //                     <Typography component="span" variant="h4">
+        //                         P.L.M.S
+        //                     </Typography>
+        //                     <Divider className={classes.nlogo} variant="middle" orientation="vertical" />
+        //                 </Box>
+        //                 <Box display="flex" flex="5" className={classes.mnav} alignItems="center">
+        //                     <Typography component="span" variant="h6">
+        //                         &nbsp;&nbsp;&nbsp;Algerial RDPP
+        //                     </Typography>
+        //                 </Box>
+        //                 <Box flex="4">
+        //                     <Tabs
+        //                         value={currentPath}
+        //                         aria-label="Navigation Tabs"
+        //                     >
+        //                         <Tab
+        //                             label={'Punch LIST'}
+        //                             component={Link}
+        //                             to="/"
+        //                             value="/"
+        //                         />
+        //                         <Tab
+        //                             label={'Management'}
+        //                             component={Link}
+        //                             to="/admin"
+        //                             value="/admin"
+        //                         />
+        //                         <Tab
+        //                             label={'Code'}
+        //                             component={Link}
+        //                             to="/code"
+        //                             value="/code"
+        //                         />
+        //                         {/* <T  ab
+        //                             label={'Companies'}
+        //                             component={Link}
+        //                             to="/companies"
+        //                             value="/companies"
+        //                         />
+        //                         <Tab
+        //                             label={'Deals'}
+        //                             component={Link}
+        //                             to="/deals"
+        //                             value="/deals"
+        //                         /> */}
+        //                     </Tabs>
+        //                 </Box>
+        //                 <Box display="flex">
+        //                     <LoadingIndicator />
 
                             
-                            {/* logout :  redirect login page */}
-                            <UserMenu logout={<Logout button />} />
-                        </Box>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </nav>
+        //                     {/* logout :  redirect login page */}
+        //                     <UserMenu logout={<Logout button />} />
+        //                 </Box>
+        //             </Box>
+        //         </Toolbar>
+        //     </AppBar>
+        // </nav>
+        <>
+         <header className={classes.root}>
+            <div>
+            <h1>
+                <a href="#">P.L.M.S</a>
+            </h1>
+            <a href="#" className="navUsername">
+                <p>Gil Dong Hong가나다라마바사아자차</p>
+            </a>
+            <select name="#">
+                <option value="Algeria">Algeria RDPP</option>
+                <option value="second">Second</option>
+                <option value="third">Third</option>
+            </select>
+            </div>
+            <nav id="gnb">
+            <ul>
+                <li>
+                <a href="#">Punch List</a>
+                </li>
+                <li className="on">
+                <a href="#/admin">Management</a>
+                </li>
+                <li>
+                <a href="#/code">Code</a>
+                </li>
+            </ul>
+            </nav>
+        </header>
+        </>
     );
 };
 
